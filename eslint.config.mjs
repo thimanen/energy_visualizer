8b32aaf5755bc7e9a93e4210ba86a638d73eb109
Teemu Himanen <thimanen@gmail.com>
2025-05-24 17:25:19 +0300

    chartByHour component - initial

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+import reactNative from "eslint-plugin-react-native";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended", "plugin:react/recommended"),
+
+    plugins: {
+        react,
+        "react-native": reactNative,
+    },
+
+    languageOptions: {
+        globals: {
+            ...reactNative.environments["react-native"]["react-native"],
+        },
+
+        ecmaVersion: 2018,
+        sourceType: "module",
+
+        parserOptions: {
+            parser: "@babel/eslint-parser",
+            requireConfigFile: false,
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+    },
+}]);
