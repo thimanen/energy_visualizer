089a5fe16f9abbf7c2f9ef4d497cf2cb9f407a0d
Teemu Himanen <thimanen@gmail.com>
2025-05-30 16:49:01 +0300

    dataCache in-use

--- src/hooks/useHourlyData.js
+++ src/hooks/useHourlyData.js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { DateTime } from 'luxon'
 
 const useHourlyData = (date) => {
   const [hourlyData, setHourlyData] = useState([])
   const [loading, setLoading] = useState(false)
+  const dataCache = useRef({})
 
   const url = `http://192.168.68.119:3000/energy/hourly/${date}`
 
@@ -11,10 +12,17 @@ const useHourlyData = (date) => {
     if (!DateTime.fromFormat(date, 'yyyy-MM-dd').isValid) {
       return
     }
+
+    if (dataCache.current[date]) {
+      setHourlyData(dataCache.current[date])
+      return
+    }
+
     setLoading(true)
 
     const response = await fetch(url)
     const json = await response.json()
+    dataCache.current[date] = json
 
     setLoading(false)
     setHourlyData(json)
